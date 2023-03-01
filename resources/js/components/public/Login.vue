bf6a72849e846902917e973536c9c5e9ced2055c
Dagzu <alexander@xtendly.com>
2023-03-01 22:00:23 +0800

    revamp login page

--- resources/js/components/public/Login.vue
+++ resources/js/components/public/Login.vue
@@ -1,34 +1,34 @@
 <template>
     <div class="login-container">
-        <div class="col-sm-8" style="height: 100%; background: rgba(0,0,0,0.5); padding: 70px">
-            <router-link class="btn btn-sm btn-light" :to="{name: 'Home'}"><i class="far fa-home"></i> Home</router-link>
-            <div class="text-center"><img :src="'/images/Logo.png'" alt="Logo" width="150"></div>
-            <h2 class="text-center text-white"><strong>DOST-SEI</strong></h2>
-            <h2 class="text-center text-white"><strong>Project Management Information System</strong></h2>
-            <div class="d-flex justify-content-center">
-                <div class="col-sm-8">
-                    <div class="card">
-                        <div class="card-body">
-                            <p>Sign in here!</p>
-                            <div class="alert alert-warning" role="alert" v-if="error">
-                                <strong><i class="far fa-exclamation-triangle"></i></strong> {{error}}
+        <div class="card shadow">
+            <div class="card-body">
+                <router-link class="btn btn-sm btn-light" :to="{name: 'Home'}"><i class="far fa-arrow-left"></i> Home</router-link>
+                <div class="text-center"><img :src="'/images/Logo.png'" alt="Logo" width="150"></div>
+                <h2 class="text-center"><strong>DOST-SEI</strong></h2>
+                <h2 class="text-center mt-2">Project Management Information System</h2>
+                <div class="d-flex justify-content-center">
+                    <div class="card w-100 mt-4">
+                            <div class="card-body">
+                                <p class="text-success font-weight-bold">Sign in here!</p>
+                                <div class="alert alert-warning" role="alert" v-if="error">
+                                    <strong><i class="far fa-exclamation-triangle"></i></strong> {{error}}
+                                </div>
+                                <form @submit.prevent="submit()">
+                                    <div class="form-floating mb-3 mt-3">
+                                        <input type="email" class="form-control" id="email" placeholder="email" v-model="form.email">
+                                        <label for="email">Email address</label>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input type="password" class="form-control" id="password" placeholder="password" v-model="form.password">
+                                        <label for="password">Password</label>
+                                    </div>
+                                    <div class="d-flex justify-content-end">
+                                        <button v-if="!loading" type="submit" class="btn btn-primary rounded-pill" style="min-width: 100px">Login</button>
+                                        <button v-else class="btn btn-secondary rounded-pill"><i class="fas fa-spinner fa-spin"></i> Loading</button>
+                                    </div>
+                                </form>
                             </div>
-                            <form @submit.prevent="submit()">
-                                <div class="form-floating mb-3 mt-3">
-                                    <input type="email" class="form-control" id="email" placeholder="email" v-model="form.email">
-                                    <label for="email">Email address</label>
-                                </div>
-                                <div class="form-floating mb-3">
-                                    <input type="password" class="form-control" id="password" placeholder="password" v-model="form.password">
-                                    <label for="password">Password</label>
-                                </div>
-                                <div class="d-flex justify-content-end">
-                                    <button v-if="!loading" type="submit" class="btn btn-primary rounded-pill" style="min-width: 100px">Login</button>
-                                    <button v-else class="btn btn-secondary rounded-pill"><i class="fas fa-spinner fa-spin"></i> Loading</button>
-                                </div>
-                            </form>
                         </div>
-                    </div>
                 </div>
             </div>
         </div>
